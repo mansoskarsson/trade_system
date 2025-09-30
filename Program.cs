34eb3cc2fb85521a0fdd7bd38d90de725b7b126a
mansoskarsson <mansoskarsson@hotmail.com>
2025-09-30 17:12:33 +0200

    Klar med ändring inlogg och utlogg
    Nu kan man skapa användare och logga in. samt logga ut och skapa en ny.
    Har lyckats få txt filen att spara email och lösenord för första
    användare med inte så att den är kvar efter jag loggat ut och skapat ny
    användare.

--- Program.cs
+++ Program.cs
@@ -16,107 +16,119 @@ using App;
 
 
 
-List<User> users = new List<User>();            //Här lägger jag till en lista med folks inlogg.
+List<User> users = new List<User>();            
 
-Console.WriteLine("=== Create an account ===");
-Console.Write("Type your Email: ");
-string newUsername = Console.ReadLine();
 
-Console.Write("Choose a password: ");
-string newPassword = Console.ReadLine();
-
-users.Add(new User(newUsername, newPassword));
-Console.WriteLine("Account created!");
-
-Console.WriteLine("=== Log in ===");
-Console.Write("Email: ");
-string loginUser = Console.ReadLine();
-
-Console.Write("Password: ");
-string loginPassword = Console.ReadLine();
 
 List<Item> items = new List<Item>();
 
 
 
 
-
- 
-
 bool running = true;
+
 
 while (running)
 {
+    Console.WriteLine("=== Create an account ===");   //Här lägger jag till så att man kan skapa en inloggning som man sedan ska kunna använda för att logga in
+    Console.Write("Type your Email: ");
+    string newUsername = Console.ReadLine();
+
+    Console.Write("Choose a password: ");
+    string newPassword = Console.ReadLine();
+
+    User newUser = new User(newUsername, newPassword);
+    users.Add(newUser);
+    Console.WriteLine("Account created!");
+
     bool loggedIn = false;
+    User currentUser = null;
 
-    foreach (User user in users)
+    while (!loggedIn)
     {
-        if (user.TryLogin(loginUser, loginPassword))
+        Console.WriteLine("=== Log in ===");
+        Console.Write("Email: ");
+        string loginUser = Console.ReadLine();
+
+        Console.Write("Password: ");
+        string loginPassword = Console.ReadLine();
+
+        foreach (User user in users)                       //kollar så att inlogget finns
         {
-            loggedIn = true;
-            break;
-        }
-    }
-    if (!loggedIn)
-    {
-        Console.WriteLine("Wrong username or password");
-    }
-    else            
-    {
-        Console.WriteLine($" Welcome {loginUser}");
-        
-
-        if (true)         // En meny för att välja vad man vill göra
-        {
-            Console.WriteLine("Ready to start trade?");
-            Console.WriteLine("Select what you want to do: ");
-            Console.WriteLine("1. Add an item");
-            Console.WriteLine("2. Show items in inventory");
-            Console.WriteLine("3. Make a trade");
-            Console.WriteLine("4. Browse trade requests");
-            Console.WriteLine("5. Browse completed trades");
-
-            string userChoice = Console.ReadLine();
-
-            switch (userChoice)
+            if (user.TryLogin(loginUser, loginPassword))
             {
-                case "1":    // Detta är för att kunna lägga till items med namn och beskrivning
-                    Item newItem = new Item("Name", "Description");
-                    Console.Write("Please type in the name of your item: ");
-                    newItem.Item_name = Console.ReadLine();
-                    Console.Write("Please type an description of your item: ");
-                    newItem.Item_description = Console.ReadLine();
-                    items.Add(newItem);
-                    Console.WriteLine("The Item have been added! Press ENTER");
-                    Console.ReadLine();
-                    break;
+                loggedIn = true;
+                currentUser = user;
+                Console.WriteLine($" Welcome {loginUser}");        // om rätt inlogg kommer man vidare till menyn
 
-                case "2":                                         // Detta är för att kunna visa vilka items som har lagt till i inventory
-                    Console.WriteLine("Items in inventory");
-                    if (items.Count == 0) {
-                        Console.WriteLine("Inventory is empty");
-                    }
-                    else
-                    {
-                        for (int i = 0; i < items.Count; i++)
-                        {
-                            Item u = items[i];
-                            Console.WriteLine($"{i + 1}. {u.Item_name} with the description: {u.Item_description}");
-                        }
-                        Console.WriteLine("Press enter to continue");
-                        Console.ReadLine();
-                    }
-                    break;
+                string[] User = { loginUser, loginPassword };     // Spara inlogg i users.txt
+                File.WriteAllLines("users.txt", User);
 
-                case "3":
-                    Trade newTrade = new Trade("Sender", "Reciever", "TradeStatus", "Item");
-                    //Console.Write("Current sender is:" {GetUser} "and current reciever is:" {GetUser});
-                    break; 
-                    
+                break;
             }
+        }
+        if (!loggedIn)
+        {
+            Console.WriteLine("Wrong username or password");           //om fel inloggningsuppgifter blir användaren tillsagd att det är fel lösenord eller username
+        }
+    }
+    bool Menu = true;
+    while (Menu)    // En meny för att välja vad man vill göra
+    {
+        Console.WriteLine("Ready to start trade?");
+        Console.WriteLine("Select what you want to do: ");
+        Console.WriteLine("1. Add an item");
+        Console.WriteLine("2. Show items in inventory");
+        Console.WriteLine("3. Make a trade");
+        Console.WriteLine("4. Browse trade requests");
+        Console.WriteLine("5. Browse completed trades");
+        Console.WriteLine("6. Logout");
+
+        string userChoice = Console.ReadLine();
+
+        switch (userChoice)
+        {
+            case "1":    // Detta är för att kunna lägga till items med namn och beskrivning
+                Item newItem = new Item("Name", "Description");
+                Console.Write("Please type in the name of your item: ");
+                newItem.Item_name = Console.ReadLine();
+                Console.Write("Please type an description of your item: ");
+                newItem.Item_description = Console.ReadLine();
+                items.Add(newItem);
+                Console.WriteLine("The Item have been added! Press ENTER");
+                Console.ReadLine();
+                break;
+
+            case "2":                                         // Detta är för att kunna visa vilka items som har lagt till i inventory
+                Console.WriteLine("Items in inventory");
+                if (items.Count == 0)
+                {
+                    Console.WriteLine("Inventory is empty");
+                }
+                else
+                {
+                    for (int i = 0; i < items.Count; i++)
+                    {
+                        Item u = items[i];
+                        Console.WriteLine($"{i + 1}. {u.Item_name} with the description: {u.Item_description}");
+                    }
+                    Console.WriteLine("Press enter to continue");
+                    Console.ReadLine();
+                }
+                break;
+
+            case "3":
+                Trade newTrade = new Trade("Sender", "Reciever", "TradeStatus", "Item");
+                //Console.Write("Current sender is:" {GetUser} "and current reciever is:" {GetUser});
+                break;
+
+            case "6":
+                Console.WriteLine("You have been logged out");
+                Menu = false;
+                break;
 
         }
-        
+
     }
     
 }
